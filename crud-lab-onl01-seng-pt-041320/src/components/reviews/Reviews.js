7a4a7a9f3436320f3ebda2b725df8adc44ce2eb3
OrkunSA <c.orkun.s@gmail.com>
2021-01-12 19:25:23 -0600

    adding solution

--- crud-lab-onl01-seng-pt-041320/src/components/reviews/Reviews.js
+++ crud-lab-onl01-seng-pt-041320/src/components/reviews/Reviews.js
@@ -1,14 +1,22 @@
-import React, { Component } from 'react';
-import Review from './Review';
+import React, { Component } from "react";
+import Review from "./Review";
 
 class Reviews extends Component {
   render() {
+    const { reviews, restaurantId, deleteReview } = this.props;
+    const restaurantsReviews = reviews.filter(
+      (review) => review.restaurantId === restaurantId
+    );
+
+    const reviewList = restaurantsReviews.map((review, index) => (
+      <Review key={index} deleteReview={deleteReview} review={review} />
+    ));
     return (
-      <ul>
-        Reviews
-      </ul>
+      <div>
+        <ul>{reviewList}</ul>
+      </div>
     );
   }
-};
+}
 
 export default Reviews;
